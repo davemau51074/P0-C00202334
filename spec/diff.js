dffabacb146787b8f7b0208f74f86a199b7395ee
zjacques <zachary.jacques160@gmail.com>
2018-10-02 12:39:06 +0100

    Fixed the test and filter to function properly enough

--- spec/diff.js
+++ spec/diff.js
@@ -12,7 +12,7 @@ describe('diff()', function () {
   });
 
   it('does something', function () {
-    expect(true).to.equal(false);
+    expect(diff([4,2,8,3,9,4,10,5,11,6])).to.deep.equal([4,2,6,5,6,5,6,5,6,5]);
   });
 
   it('does something else', function () {
