a18721e6358eaf8c21dda87442c0c5d68a613439
David Nolan <davidnln13@yahoo.com>
2018-10-03 17:18:45 +0100

    fixed diff

--- diff.js
+++ diff.js
@@ -2,24 +2,20 @@
 'use strict';
 
 
-var diff = function () {
+var diff = function (x) {
 
-  const input =[4,2,8,3,9,4,10,5,11,6];
-  const output = [4,2,6,5,6,5,6,5,6,5];
+  //const input =[4,2,8,3,9,4,10,5,11,6];
+  //const output = [4,2,6,5,6,5,6,5,6,5];
 
-  var x = 0;
-  var y = 0;
-  var ans = 0;
-  // your code goes here
-  for(var i = 0; i <= input.length; i++)
+  var ans = [];
+  var previous  = 0;
+
+  for(var i = 0; i < x.length; i++)
   {
-      if(x > y );
-      {
-        ans=y-x;
-      }
-
+    ans.push(Math.abs(x[i] - previous))
+    previous  = x[i]
   }
-
+  return ans
 }
 
 if ( typeof module !== "undefined" ) {
